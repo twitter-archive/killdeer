582c267ad1d8722a9b1a55c4502b22b56ddce6fd
Marcel Molina <marcel@twitter.com>
2010-08-03 15:55:44 -0700

    Print out transaction id header if it's provided.

--- src/main/scala/com/twitter/killdeer/ResponseSampleServlet.scala
+++ src/main/scala/com/twitter/killdeer/ResponseSampleServlet.scala
@@ -3,6 +3,10 @@ package com.twitter.killdeer
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
 class ResponseSampleServlet(responseSampleFilename: String) extends HttpServlet {
+  def txnid(req: HttpServletRequest) = req.getHeader("X-Transaction-Id") match {
+    case null => "-"
+    case s => s
+  }
   val sample = new ResponseSampleLoader(responseSampleFilename)
 
   override def doGet(req: HttpServletRequest, res: HttpServletResponse) {
@@ -11,5 +15,7 @@ class ResponseSampleServlet(responseSampleFilename: String) extends HttpServlet 
     res.setContentLength(response.size)
     res.setStatus(response.status)
     res.getWriter().write("." * response.size)
+
+    println(response.latencyMs.toString() + " " + txnid(req))
   }
 }
