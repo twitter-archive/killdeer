ec074f89be43218a2f3deb1b4d16615f33236770
Marcel Molina <marcel@twitter.com>
2010-08-23 15:22:50 -0700

    new txnid and request-id headers plus BONUS whitespace

--- src/main/scala/com/twitter/killdeer/ResponseSampleServlet.scala
+++ src/main/scala/com/twitter/killdeer/ResponseSampleServlet.scala
@@ -4,10 +4,16 @@ import org.eclipse.jetty.continuation.ContinuationSupport
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
 class ResponseSampleServlet(responseSampleFilename: String) extends HttpServlet {
-  def txnid(req: HttpServletRequest) = req.getHeader("X-Transaction-Id") match {
+  def txnid(req: HttpServletRequest) = req.getHeader("X-Txn-Id") match {
     case null => "-"
     case s => s
   }
+
+  def rqid(req: HttpServletRequest) = req.getHeader("X-Rq-Id") match {
+    case null => "-"
+    case s => s
+  }
+
   val sample = new ResponseSampleLoader(responseSampleFilename)
 
   override def doGet(req: HttpServletRequest, res: HttpServletResponse) {
@@ -22,6 +28,7 @@ class ResponseSampleServlet(responseSampleFilename: String) extends HttpServlet 
     //} else { // if (continuation.isExpired) {
       //val response = req.getAttribute("response").asInstanceOf[Response]
       //res.setContentLength(response.size)
+
       res.setStatus(200)
       //res.getWriter().write("." * response.size)
     //}
