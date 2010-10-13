89882a4f67348e69fab3890880b60b47c099ac07
marius a. eriksen <marius@twitter.com>
2010-10-12 17:43:08 -0700

    be more careful with futures in the throttling handler.

--- src/main/scala/com/twitter/killdeer/ThrottlingHandler.scala
+++ src/main/scala/com/twitter/killdeer/ThrottlingHandler.scala
@@ -1,6 +1,8 @@
 package com.twitter.killdeer
 
 import scala.math
+import scala.collection.mutable.ListBuffer
+import scala.collection.JavaConversions._
 
 import java.util.concurrent.TimeUnit
 
@@ -10,7 +12,8 @@ import org.jboss.netty.handler.queue.BufferedWriteHandler
 import org.jboss.netty.util.{Timer, TimerTask, Timeout}
 
 abstract class BufferingThrottler(rateBps: Int, timer: Timer) {
-  val buffer = ChannelBuffers.dynamicBuffer()
+  var events = List[MessageEvent]()
+  var ctx: ChannelHandlerContext = _
 
   // Minimum granularity: 10ms.
   val rateBpms = rateBps.toFloat / 1000.0
@@ -23,35 +26,68 @@ abstract class BufferingThrottler(rateBps: Int, timer: Timer) {
       if (to.isCancelled)
         return
 
-      val channelBuffer =
-        buffer.synchronized {
-          if (buffer.readableBytes > 0)
-            Some(buffer.readBytes(math.min(bytesPerTick, buffer.readableBytes)))
-          else
-            None
-        }
-
-      channelBuffer.foreach(write)
+      doWrite(bytesPerTick)
       timeout = timer.newTimeout(this, tickMs, TimeUnit.MILLISECONDS)
     }
   }
 
   timeout = timer.newTimeout(task, tickMs, TimeUnit.MILLISECONDS)
 
-  def sink(in: ChannelBuffer) {
-    buffer.synchronized {
-      buffer.writeBytes(in)
+  def sink(ctx: ChannelHandlerContext, e: MessageEvent) {
+    assert(e.getMessage.isInstanceOf[ChannelBuffer])
+    this.ctx = ctx
+
+    synchronized {
+      events = events :+ e
     }
   }
 
   def stop() = {
     timeout.cancel()
-    if (buffer.readableBytes > 0)
-      write(buffer)
+    doWrite(Int.MaxValue)
   }
 
+  def doWrite(howmuch: Int) = {
+    val writeBuffers = new ListBuffer[ChannelBuffer]()
+    val writeFutures = new ListBuffer[ChannelFuture]()
+
+    synchronized {
+      var written = 0
+
+      while (written < howmuch && !events.isEmpty) {
+        var event = events.head
+        val buffer = event.getMessage.asInstanceOf[ChannelBuffer]
+    
+        val toWrite = math.min(howmuch - written, buffer.readableBytes)
+        written += toWrite
+
+        writeBuffers.append(buffer.readBytes(toWrite))
+        if (!buffer.readable) {
+          events = events.tail
+          writeFutures.append(event.getFuture)
+        }
+      }
+    }
+
+    if (!writeBuffers.isEmpty) {
+      val composite = ChannelBuffers.wrappedBuffer(writeBuffers.toArray:_*)
+
+      val future = Channels.future(ctx.getChannel)
+      future.addListener(new ChannelFutureListener() {
+        def operationComplete(future: ChannelFuture) {
+          if (future.isSuccess)
+            writeFutures.foreach(_.setSuccess())
+          else
+            writeFutures.foreach(_.setFailure(future.getCause))
+        }
+      })
+      write(composite, future)
+    }
+  }
+
+
   // Implement this!
-  def write(out: ChannelBuffer)
+  def write(out: ChannelBuffer, future: ChannelFuture)
 }
 
 // These need to be inserted very low in the stack-- it needs access
@@ -62,24 +98,21 @@ extends SimpleChannelDownstreamHandler
   var ctx: ChannelHandlerContext = _
 
   val throttler = new BufferingThrottler(rateBps, timer) {
-    def write(out: ChannelBuffer)  {
-      if (ctx ne null) {
-        // We don't care about the results.
-        val future = Channels.future(ctx.getChannel)
+    def write(out: ChannelBuffer, future: ChannelFuture)  {
+      if (ctx ne null)
         Channels.write(ctx, future, out)
-      }
     }
   }
 
-  override def writeRequested(ctx: ChannelHandlerContext, e: MessageEvent) {
+  override def writeRequested(ctx: ChannelHandlerContext, e: MessageEvent) = {
     this.ctx = ctx
-    e.getMessage match {
-      case buffer: ChannelBuffer => throttler.sink(buffer)
-      case _ => super.writeRequested(ctx, e)
-    }
+    throttler.sink(ctx, e)
   }
 
   override def closeRequested(ctx: ChannelHandlerContext, e: ChannelStateEvent) {
+    // The upstream handler waits for the write futures to complete
+    // before closing, so we really shouldn't have any issues with
+    // buffering here.
     throttler.stop()
     ctx.sendUpstream(e)
   }
